4927d6a1899e343b565a97c0ba2c058ba91473c4
SteffenTunkel <48067167+SteffenTunkel@users.noreply.github.com>
2019-08-07 16:54:12 +0200

    not working update

--- find_background.py
+++ find_background.py
@@ -1,22 +1,51 @@
 import create_db
 import statistics
-
+# INTEGER PRIMARY KEY bei der anderen Funktion für frame_no in der time table
+	#Laserid und azimuth nicht real sondern int
+	#allgemeine einstellungen in eine inidatei auslagern
 def find_background(db_name):
-    max_azimuth_value=36000
-		max_laser_id_value=15
-		conn = sqlite3.connect(db_name)
-		c = conn.cursor()
-		
+	max_azimuth_value=36000
+	max_laser_id_value=15
+	conn = sqlite3.connect(db_name)
+	c = conn.cursor()
+	
+	sql_command_to_drop = "DROP TABLE IF EXISTS time"    #1234
+	c.execute(sql_command_to_drop)    #1234
+    
+	sql_command_to_create = """CREATE TABLE background(
+                azimuth INTEGER NOT NULL,
+		laser_id INTEGER NOT NULL,
+                median_dist REAL,
+		PRIMARY KEY(azimuth, laser_id)
+		)"""
+	c.execute(sql_command_to_create)
+
+	conn.commit()
+	
 		#sql_command_to_select= "SELECT * FROM main WHERE priority=?"
     #c.execute("SELECT * FROM tasks WHERE priority=?", (priority,))
  
     #rows = cur.fetchall()
 		
-		for i_azimuth in range(max_azimuth_value):
-			for i_laser_id in range(-max_laser_id_value, max_laser_id_value):	
-				c.execute('SELECT * FROM main WHERE azimuth=? AND laser_id=?', (i_azimuth, i_laser_id)
-				points = c.fetchall()
-				print(points)
-				#ToDo auf distance zugreifen
-				#median_distance=statistics.median(points)
-				
+	for i_azimuth in range(max_azimuth_value):
+		for i_laser_id in range(-max_laser_id_value, max_laser_id_value):	
+			c.execute('SELECT * FROM main WHERE azimuth=? AND laser_id=?', (i_azimuth, i_laser_id)
+			points = c.fetchall()
+			print(points)
+			#ToDo auf distance zugreifen
+			#median_distance=statistics.median(distances)
+		
+				  
+			sql_command_to_fill = 'INSERT INTO background VALUES('
+			sql_command_to_fill += str(i_azimuth) + ','
+			sql_command_to_fill += str(i_laser_id) + ','
+			sql_command_to_fill += str(median_distance) + ')'
+
+            		c.execute(sql_command_to_fill)
+				  
+	conn.commit()
+
+def compare_with_background():
+# ähnlich zu background finden, alle durchgehen und die einträge mit passendem wert löschen, Toleranz beachten.				  
+				 
+				  
