4548b1851c61ac26376a9d0cd166244c235602c2
SteffenTunkel <48067167+SteffenTunkel@users.noreply.github.com>
2019-08-28 16:36:22 +0200

    Update find_background.py

--- find_background.py
+++ find_background.py
@@ -6,6 +6,8 @@ import time
 import init
 
 def find_background(db_name):
+	tot_time = time.time() #DEBUG
+	R_time=time.time() #DEBUG
 
 	# open db and create a new table
 	conn = sqlite3.connect(db_name)
@@ -21,28 +23,61 @@ def find_background(db_name):
 	c.execute(sql_command_to_create)
 	conn.commit()
 
+	# get values from main
+	c.execute("SELECT * FROM main")
+	data_array = c.fetchall()
 
-	# for each combination of azimuth and laser_id
-	i_azimuth=0
-	i_laser_id=0
+	print('R time: ' + str(time.time()-R_time)) #DEBUG
+	A_time=time.time() #DEBUG
+
+	# sort main array by azimuth
+	sort_array = sorted(data_array, key=lambda x: x[7])
+
+	print('A time: ' + str(time.time()-A_time)) #DEBUG
+
+	break_flag = False
+	main_id = 0
+	t = 500 #DEBUG
+	five_time=time.time() #DEBUG
 	for i_azimuth in range(init.max_azimuth_value):
-		for i_laser_id in range(-init.max_laser_id_value, init.max_laser_id_value):
-			
-			# get all the fitting records
-			c.execute("SELECT * FROM main WHERE azimuth=:i_azimuth AND laser_id=:i_laser_id"
-						, {"i_azimuth": i_azimuth, "i_laser_id": i_laser_id})
-			data_array = c.fetchall()
+		if i_azimuth == t: #DEBUG
+			print(str(i_azimuth) + '\t' + str(time.time()-five_time)) #DEBUG
+			t = t + 500 #DEBUG
+			five_time=time.time() #DEBUG
+
+		same_az_array = []
+		same_azimuth_flag = True
+		while same_azimuth_flag == True:
+			if sort_array[main_id][7] == i_azimuth:
+				same_az_array.append(sort_array[main_id])
+				main_id = main_id + 1
+				if main_id == len(sort_array):
+					break_flag = True
+					break
+			else:
+				same_azimuth_flag = False
+
+		if break_flag == True:
+			break
+
+		for i_laser_id in range(0, init.max_laser_id_value+1):
+			same_lid_array = []
+			for m in range(len(same_az_array)):
+				if same_az_array[m][6] == i_laser_id:
+					same_lid_array.append(same_az_array[m])
+
+
+
 
 			# calculate the median
 			distance_array = []
 			median_distance = 0
-			for n in range(len(data_array)):
-				distance_array.append(data_array[n][8])
+			for j in range(len(same_lid_array)):
+				distance_array.append(same_lid_array[j][8])
 			try:
 				median_distance = statistics.median(distance_array)
 			except:
 				median_distance = 0.0
-
 			# write the median into the new table
 			if median_distance != 0.0:
 				sql_command_to_fill = 'INSERT INTO background VALUES('
@@ -54,18 +89,24 @@ def find_background(db_name):
 				sql_command_to_fill += str(i_azimuth) + ','
 				sql_command_to_fill += str(i_laser_id) + ','
 				sql_command_to_fill += "NULL" + ')'		
+			
+			c.execute(sql_command_to_fill)		
+	conn.commit()
+		#print('F time: ' + str(time.time()-E_time))
 
-			c.execute(sql_command_to_fill)
-			conn.commit()
-      
+	print('total time:'+ str(time.time()-tot_time)) #DEBUG
 
 
 
 
-def compare_with_background():
+
+
+
+def compare_with_background(db_name):
+	tot_time = time.time() #DEBUG
 
 	# connect to the database
-	conn = sqlite3.connect('data.db')
+	conn = sqlite3.connect(db_name)
 	c = conn.cursor()
 	
 	# create a new table for the not background, means moving objects
@@ -87,26 +128,72 @@ def compare_with_background():
 	c.execute(sql_command_to_create)
 	conn.commit()
 
+
+
+
+
+	# get values from main
+	R_time=time.time() #DEBUG
+	c.execute("SELECT * FROM main")
+	main_array_unsorted = c.fetchall()
+
+	print('R time: ' + str(time.time()-R_time)) #DEBUG
+	A_time=time.time() #DEBUG
+
+	# sort main array by azimuth
+	main_array = sorted(main_array_unsorted, key=lambda x: x[7])
+
+	print('A time: ' + str(time.time()-A_time)) #DEBUG'
+
+
+	# get values from background
+	R_time=time.time() #DEBUG
+	c.execute("SELECT * FROM background")
+	background_array = c.fetchall()
+
+	print('R time: ' + str(time.time()-R_time)) #DEBUG
+
+
+
+
+	same_azimuth_flag = True
+	break_flag = False
 	i_azimuth = 0
 	i_laser_id = 0
 	i_key = 0
-	for i_azimuth in range(init.max_azimuth_value):
-		for i_laser_id in range(-init.max_laser_id_value, init.max_laser_id_value):
-			# get all the data for the fitting azimuth/laser_id combination
-			c.execute("SELECT * FROM main WHERE azimuth=:i_azimuth AND laser_id=:i_laser_id"
-					, {"i_azimuth": i_azimuth, "i_laser_id": i_laser_id})
-			main_array = c.fetchall()
 
-			# get the median value
-			c.execute("SELECT * FROM background WHERE azimuth=:i_azimuth AND laser_id=:i_laser_id"
-					, {"i_azimuth": i_azimuth, "i_laser_id": i_laser_id})
-			background_array = c.fetchall()
-			median = background_array[0][2]
+	for i in range (len(background_array)):
+		#B_time = time.time()
+		i_az = background_array[i][0]
+		i_lid = background_array[i][1]
+		median = background_array[i][2]
 
+		same_az_array = []
+		same_azimuth_flag = True
+		n = 0
+		while same_azimuth_flag == True:
+			if main_array[n][7] == i_azimuth:
+				same_az_array.append(main_array[n])
+				n = n + 1
+				if n == len(main_array):
+					break_flag = True
+					break
+			else:
+				same_azimuth_flag = False
+
+		if break_flag == True:
+			break
+
+		for i_laser_id in range(0, init.max_laser_id_value+1):
+			same_lid_array = []
+			for m in range(len(same_az_array)):
+				if same_az_array[m][6] == i_laser_id:
+					same_lid_array.append(same_az_array[m])
+	
 			# compare the distance of each record with the median
-			for n in range(len(main_array)):
-				print(abs( main_array[n][8] - median))
-				if abs( main_array[n][8] - median) >= init.backgnd_tol:
+			for n in range(len(same_lid_array)):
+				#print(abs( same_lid_array[n][8] - median)) #DEBUG
+				if abs( same_lid_array[n][8] - median) >= init.backgnd_tol:
 					
 					# copy the data from the main record to the moving one. But with a new key.
 					sql_command_to_fill = 'INSERT INTO moving VALUES('
@@ -120,32 +207,33 @@ def compare_with_background():
 					sql_command_to_fill += str(main_array[n][7]) + ','
 					sql_command_to_fill += str(main_array[n][8]) + ','
 					sql_command_to_fill += "NULL" + ')'
-					c.execute(sql_command_to_fill)
-					
+					c.execute(sql_command_to_fill)					
 					i_key = i_key + 1
+
+		#print('B time: ' + str(time.time()-B_time)) #DEBUG'
 	conn.commit()
+	print('total time:'+ str(time.time()-tot_time)) #DEBUG
 						
-def test_find_background(db_name='data.db'):
+def test_find_background(db_name=init.db_name):
 	print('testing: find_background')
 	find_background(db_name)
 
 	conn = sqlite3.connect(db_name)
 	c = conn.cursor()
 
-	sql_command_to_select = "SELECT * FROM " + "background WHERE azimuth=12 AND laser_id=5"
 	c.execute(sql_command_to_select)
 	array=c.fetchall()
 	print(array)
 
-def test_compare_with_background(db_name='data.db'):
+def test_compare_with_background(db_name=init.db_name):
 	print('testing: compare_with_background')
 
-	compare_with_background()
+	compare_with_background(db_name)
 
 	conn = sqlite3.connect(db_name)
 	c = conn.cursor()
 	
-	output.print_table('moving')
+	#output.print_table('moving')
 	# sql_command_to_select = "SELECT * FROM " + "background WHERE azimuth=12 AND laser_id=5"
 	# c.execute(sql_command_to_select)
 	# array=c.fetchall()
