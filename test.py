90b57abfed9e33c7f18f491caba019be07dfda7e
SteffenTunkel <48067167+SteffenTunkel@users.noreply.github.com>
2019-10-08 21:22:00 +0200

    Update test.py

--- test.py
+++ test.py
@@ -3,16 +3,18 @@ import output
 import find_background
 import point_cloud
 
-#output.test_print_table('data.db', 'time')
+#output.test_print_table('moving')
 
-#output.test_get_table('data.db', 'main')
+#output.test_get_table('moving')
+
+output.test_visualize_frame_from_db(frame_no=1, source_table='moving')
 
 #create_db.test_read_files()
 
 #create_db.test_create_and_fill_database()
 
-#find_background_idea.test_find_background()
+#find_background.test_find_background()
 
-find_background.test_compare_with_background()
+#find_background.test_compare_with_background()
 
 #point_cloud.test_find_point_clouds()
